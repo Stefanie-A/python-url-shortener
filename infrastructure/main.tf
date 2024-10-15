567fd8527b24ca0122221fbca558b45d2de98944
Stefanie-A <albertstephanie630@gmail.com>
2024-10-15 13:05:00 +0100

    fix: key pair

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -63,6 +63,7 @@ resource "aws_instance" "web_server" {
   ami                         = "ami-0866a3c8686eaeeba"
   instance_type               = "t2.micro"
   vpc_security_group_ids      = [aws_security_group.instance.id]
+  key_name = aws_key_pair.ssh-key.key_name
   user_data                   = <<-EOF
                 #!/bin/bash
                 sudo apt update -y

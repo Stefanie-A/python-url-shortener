273c7fffef802c7b0d9da1bdd2fe2fc1b003d76a
Stefanie-A <albertstephanie630@gmail.com>
2025-02-13 20:25:26 +0000

    fix: terraform_version

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -5,7 +5,7 @@ terraform {
       version = "~>5.0"
     }
   }
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.10.4"
   # backend "s3" {
   #   bucket = "terraform-state21125"
   #   key    = "terraform.tfstate"

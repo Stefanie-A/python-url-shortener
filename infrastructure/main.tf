8d586f57a654e3f56c23d901959930b492d1ac75
Stefanie-A <albertstephanie630@gmail.com>
2025-02-17 18:05:33 +0000

    fix: api gateway

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -187,29 +187,29 @@ resource "aws_api_gateway_rest_api" "api_gateway" {
 }
 
 resource "aws_api_gateway_resource" "api_resource" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
   path_part   = "shorten"
 }
 
 resource "aws_api_gateway_method" "method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
   resource_id   = aws_api_gateway_resource.api_resource.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
+  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
   resource_id             = aws_api_gateway_resource.api_resource.id
   http_method             = aws_api_gateway_method.method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.lambda-func.invoke_arn
+  uri                     = aws_lambda_function.lambda_func.invoke_arn
 }
 
 resource "aws_api_gateway_method_response" "response" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
   resource_id = aws_api_gateway_resource.api_resource.id
   http_method = aws_api_gateway_method.method.http_method
   status_code = "200"
@@ -221,7 +221,7 @@ resource "aws_lambda_permission" "apigw_lambda" {
   function_name = var.lambda_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+  source_arn = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*/*"
 }
 
 resource "aws_api_gateway_api_key" "api_key" {

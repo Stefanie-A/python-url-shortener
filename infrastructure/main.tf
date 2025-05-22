738a0d198e0369920992e7fc6e2f1451fe79a283
Stefanie-A <albertstephanie630@gmail.com>
2025-05-22 21:20:12 +0100

    fix: ECS task execution iam role

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -240,24 +240,21 @@ resource "aws_ecs_cluster" "ecs_cluster" {
 
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Action: [
-        "sts:AssumeRole",
-        "ecr:GetDownloadUrlForLayer",
-        "ecr:BatchGetImage",
-        "ecr:GetAuthorizationToken",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-        ],
-      Effect = "Allow",
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
       }
-    }]
+    ]
   })
 }
+
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name

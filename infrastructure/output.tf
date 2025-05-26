d48bbce278b4616ed42382589dbc2ed3a4e66640
Stefanie-A <albertstephanie630@gmail.com>
2025-05-26 17:18:43 +0100

    chore: delete ecs cluster

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -14,7 +14,3 @@ output "ecr_repository_url" {
   value       = aws_ecr_repository.ecr_repository.repository_url
 }
 
-output "alb_dns_name" {
-  description = "The DNS name of the Application Load Balancer."
-  value       = aws_lb.main_alb.dns_name
-}
